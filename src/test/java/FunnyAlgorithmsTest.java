a1818efc7ad19362d29eddbfb30ccd0ff6e97b98
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2022-10-28 15:23:46 +0200

    Added Before Method

---
+++ src/test/java/FunnyAlgorithmsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class FunnyAlgorithmsTest
+{
+    private FunnyAlgorithms funnyAlgorithms;
+
+    @Before
+    public void before()
+    {
+        funnyAlgorithms = new FunnyAlgorithms();
+    }
+}
