7fb7c620abf4179a7d17af0d4e5af6f6d4513a3a
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2022-10-28 17:03:52 +0200

    Modified timing

--- src/test/java/FunnyAlgorithmsTest.java
+++ src/test/java/FunnyAlgorithmsTest.java
@@ -11,6 +11,7 @@ public class FunnyAlgorithmsTest
     private FunnyAlgorithms funnyAlgorithms;
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+
     @Before
     public void before()
     {
@@ -32,22 +33,27 @@ public class FunnyAlgorithmsTest
     @Test
     public void binaryShouldFail()
     {
+        System.out.println("Test binaryShouldFail" + " " + "started at" + " " + new DateTime());
         int[] values = {10,20,30,40,50};
         for(int i = 60;i < 120;i += 10)
             assertEquals(-1,funnyAlgorithms.binarySearch(values,i));
+        System.out.println("Test binaryShouldFail" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void swapWorks()
     {
+        System.out.println("Test swapWorks" + " " + "started at" + " " + new DateTime());
         int[] first = {10,20,30};
         FunnyAlgorithms.swap(first,0,1);
         assertEquals(20,first[0]);
         FunnyAlgorithms.swap(first,2,1);
         assertEquals(30,first[1]);
+        System.out.println("Test swapWorks" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void selectionWorks()
     {
+        System.out.println("Test selectionWorks" + " " + "started at" + " " + new DateTime());
         int[] values = {60,10,30,40,50,80};
         funnyAlgorithms.selectionSort(values,1);
         boolean ascending = true;
@@ -75,18 +81,22 @@ public class FunnyAlgorithmsTest
             }
         }
         assertTrue(descending);
+        System.out.println("Test selectionSortWorks" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void selectionShouldFail()
     {
+        System.out.println("Test selectionShouldFail" + " " + "started at" + " " + new DateTime());
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("L'ordine può essere 0 o 1.");
         int[] values = {10,100,20,50,70};
         funnyAlgorithms.selectionSort(values,10);
+        System.out.println("Test selectionShouldFail" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void stringToIntWorks()
     {
+        System.out.println("Test stringToIntWorks" + " " + "started at" + " " + new DateTime());
         String[] values = {"10","30","400","200","40","80","-20","-30","-40"};
         assertEquals(10,funnyAlgorithms.stringToIntConverter(values[0]));
         assertEquals(30,funnyAlgorithms.stringToIntConverter(values[1]));
@@ -97,53 +107,66 @@ public class FunnyAlgorithmsTest
         assertEquals(-20,funnyAlgorithms.stringToIntConverter(values[6]));
         assertEquals(-30,funnyAlgorithms.stringToIntConverter(values[7]));
         assertEquals(-40,funnyAlgorithms.stringToIntConverter(values[8]));
+        System.out.println("Test stringToIntWorks" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void stringToIntNull()
     {
+        System.out.println("Test stringToIntNull" + " " + "started at" + " " + new DateTime());
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Number can't be null");
         String number = "";
         int value = funnyAlgorithms.stringToIntConverter(number);
+        System.out.println("Test stringToIntNull" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void stringToIntWithLetters()
     {
+        System.out.println("Test stringToIntWithLetters" + " " + "started at" + " " + new DateTime());
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Number can be composed only by digits");
         String number = "10abc20";
         int value = funnyAlgorithms.stringToIntConverter(number);
+        System.out.println("Test stringToIntWithLetters" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void stringToIntWithSpecialChar()
     {
+        System.out.println("Test stringToIntWithSpecialChar" + " " + "started at" + " " + new DateTime());
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Number can be composed only by digits");
         String number = "10!|_20";
         int value = funnyAlgorithms.stringToIntConverter(number);
+        System.out.println("Test stringToIntWithSpecialChar" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void stringToIntWithSpace()
     {
+        System.out.println("Test stringToIntWithSpace" + " " + "started at" + " " + new DateTime());
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Number can be composed only by digits");
         String value = "10 20 30 40 50";
         int result = funnyAlgorithms.stringToIntConverter(value);
+        System.out.println("Test stringToIntWithSpace" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void stringToIntRangeUpper()
     {
+        System.out.println("Test stringToIntRangeUpper" + " " + "started at" + " " + new DateTime());
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Required number is out of range");
         String value = "32768";
         int result = funnyAlgorithms.stringToIntConverter(value);
+        System.out.println("Test stringToIntRangeUpper" + " " +  "ended at" + " " + new DateTime());
     }
     @Test
     public void stringToIntRangeLower()
     {
+        System.out.println("Test stringToIntRangeLower" + " " + "started at" + " " + new DateTime());
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Required number is out of range");
         String value = "-32770";
         int result = funnyAlgorithms.stringToIntConverter(value);
+        System.out.println("Test stringToIntRangeLower" + " " +  "ended at" + " " + new DateTime());
     }
 }
