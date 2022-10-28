6e1a0be6a2c4a6cf944693ba771a726f004cc950
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2022-10-28 15:31:12 +0200

    Modified Null Test method

--- src/test/java/FunnyAlgorithmsTest.java
+++ src/test/java/FunnyAlgorithmsTest.java
@@ -1,20 +1,26 @@
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class FunnyAlgorithmsTest
 {
     private FunnyAlgorithms funnyAlgorithms;
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void before()
     {
         funnyAlgorithms = new FunnyAlgorithms();
     }
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void stringToIntNull()
     {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Number can't be null");
         String number = "";
         int value = funnyAlgorithms.stringToIntConverter(number);
     }
